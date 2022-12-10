c68a127fef6ce3b238c9f97b57f4486576a80c69
giovanesouza <developergiovanesouza@gmail.com>
2022-12-10 15:30:21 -0300

    Ajustes no modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,4 +1,5 @@
 // MODAL -  INICIAR POST
+const body = document.querySelector('body');
 const modal = document.querySelector('#myModal');
 const btnModal = document.querySelector('#myBtn');
 const closeModal = document.querySelector('#close');
@@ -6,16 +7,25 @@ const closeModal = document.querySelector('#close');
 // Ativa modal para postagem
 btnModal.addEventListener('click', () => {
     modal.style.display = 'block';
+
+    // Desabilita barra de rolagem da página
+    body.style.overflow = 'hidden';
 });
 
 // Fecha o modal ao clicar no x
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
+
+    // Habilita barra de rolagem da página
+    body.style.overflow = 'initial';
 });
 
 // Fecha o modal ao clicar em qualquer área da página (exeto o próprio modal)
 window.addEventListener('click', (e) => {
-    if(e.target == modal) {
+    if (e.target == modal) {
         modal.style.display = 'none';
+
+        // Habilita barra de rolagem da página
+        body.style.overflow = 'initial';
     }
 });
